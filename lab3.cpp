a9addc7702ade8b28f0d1bb55dc3e570357d2a77
paxanDudin <SteamOriginUplay@yandex.ru>
2019-04-26 11:24:34 +0700

    3

--- lab3.cpp
+++ lab3.cpp
@@ -7,7 +7,7 @@
 #include <signal.h>
 #include <unistd.h> 
 int i = 0; 
-void* thread_func(void *arg) {
+//void* thread_func(void *arg) {
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 //	for (i=0; i < 4; i++)
 		printf("I'm still running!\n");
